2bfb2b749b92f937a2fbc508f82cfd1303396247
Николай С <nicksrnk@gmail.com>
2025-05-12 13:14:58 +0500

    Change ports

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -2,7 +2,7 @@ export let host: string = `${document.location.protocol.slice(0, -1)}://${docume
 
 if (document.location.host.split(':')[1]) {
     if (document.location.host.split(':')[1] !== '3000')
-        host += ':7788'
+        host += ':9000'
     else
         host += ':8000'
 } else
